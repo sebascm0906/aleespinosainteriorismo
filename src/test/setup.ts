7f28e3f8e56c2f24b7b37f476e2d2be727cad1db
Sebastián Cervera Maltos <117860901+sebascm0906@users.noreply.github.com>
2026-08-29 18:10:52 -0600

    test: provide scrollTo in jsdom

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,6 +1,11 @@
 import '@testing-library/jest-dom/vitest'
 import { afterEach, vi } from 'vitest'
 
+Object.defineProperty(HTMLElement.prototype, 'scrollTo', {
+  configurable: true,
+  value: vi.fn(),
+})
+
 afterEach(() => {
   vi.unstubAllGlobals()
   vi.unstubAllEnvs()
