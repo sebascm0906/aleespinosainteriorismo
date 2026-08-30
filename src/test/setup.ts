9e8bdf5fc2838424aef58ac2d82945322f037545
Sebastián Cervera Maltos <117860901+sebascm0906@users.noreply.github.com>
2026-08-29 18:12:36 -0600

    test: reset scrollTo mock between tests

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,12 +1,15 @@
 import '@testing-library/jest-dom/vitest'
 import { afterEach, vi } from 'vitest'
 
+const scrollToMock = vi.fn()
+
 Object.defineProperty(HTMLElement.prototype, 'scrollTo', {
   configurable: true,
-  value: vi.fn(),
+  value: scrollToMock,
 })
 
 afterEach(() => {
+  scrollToMock.mockClear()
   vi.unstubAllGlobals()
   vi.unstubAllEnvs()
 })
